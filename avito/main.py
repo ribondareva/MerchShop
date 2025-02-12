ca96a57be0338207318a7adaedb75c2134631302
ribondareva <msbondareva01@gmail.com>
2025-02-12 20:22:43 +0300

    update main

--- avito/main.py
+++ avito/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.responses import ORJSONResponse
 from core.config import settings
 from api import router as api_router
 from core.models import db_helper, Base
@@ -21,18 +22,18 @@ from core.models import db_helper, Base
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
     yield
     # shutdown
     print("dispose engine")
     await db_helper.dispose()
 
 
-main_app = FastAPI(lifespan=lifespan)
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
 main_app.include_router(
     api_router,
-    prefix=settings.api.prefix,
 )
 # oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 #
