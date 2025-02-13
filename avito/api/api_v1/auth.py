fda4a7b13f4c621fec5420c56e326c7f4b0f6690
ribondareva <msbondareva01@gmail.com>
2025-02-13 15:20:43 +0300

    Include auth router from fastapi-users

--- avito/api/api_v1/auth.py
+++ avito/api/api_v1/auth.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
 
+from api.api_v1.fastapi_users_router import fastapi_users
+from api.dependencies.authentification.backend import authentication_backend
 from core.config import settings
 
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
     tags=["Auth"],
+)
+
+router.include_router(
+    router=fastapi_users.get_auth_router(authentication_backend),
 )
