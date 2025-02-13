25b51231e217d7e84ebc24d68385b47683807421
ribondareva <msbondareva01@gmail.com>
2025-02-13 15:20:58 +0300

    Updates

--- avito/schemas/user.py
+++ avito/schemas/user.py
@@ -1,15 +1,19 @@
+import uuid
+from typing import Optional
+
 from pydantic import BaseModel, ConfigDict
 from uuid import UUID
+from fastapi_users import schemas
 
 
-class UserBase(BaseModel):
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    username: str
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(schemas.BaseUserCreate):
     username: str
 
 
-class UserCreate(UserBase):
-    hashed_password: str
-
-
-class UserRead(UserBase):
-    model_config = ConfigDict(from_attributes=True)
-    id: UUID
+class UserUpdate(schemas.BaseUserUpdate):
+    username: Optional[str]
