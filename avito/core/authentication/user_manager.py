fc0ac8caec2a0451f197cc1808c90a6ca1a38ef6
ribondareva <msbondareva01@gmail.com>
2025-02-13 12:20:15 +0300

    Create user manager

---
+++ avito/core/authentication/user_manager.py
@@ -0,0 +1,29 @@
+import logging
+import uuid
+from typing import Optional, TYPE_CHECKING
+from fastapi_users import BaseUserManager, UUIDIDMixin
+
+from core.config import settings
+from core.models import User
+
+
+if TYPE_CHECKING:
+    from fastapi import Request
+
+
+log = logging.getLogger(__name__)
+
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = settings.access_token.reset_password_token_secret
+    verification_token_secret = settings.access_token.verification_token_secret
+
+    async def on_after_register(
+        self,
+        user: User,
+        request: Optional["Request"] = None,
+    ):
+        log.warning(
+            "User %r has registered.",
+            user.id,
+        )
